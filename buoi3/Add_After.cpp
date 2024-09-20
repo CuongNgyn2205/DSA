8e03d17e92ec9e7b3a37866078964f0c2df5f1de
CuongNguyen <c.ncuongnguyen2708@gmail.com>
2024-09-20 16:43:47 +0700

    commited:

--- buoi3/Add_After.cpp
+++ buoi3/Add_After.cpp
@@ -1,7 +1,6 @@
 /*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
 define
 include
-[
 ###End banned keyword*/
 
 #include <iostream>
@@ -9,89 +8,93 @@ using namespace std;
 
 
 //###INSERT CODE HERE -
+using namespace std;
 struct Node{
     int info;
     Node *next;
 };
 struct List{
-    Node *Head,*Tail;
-};
-Node *GetNode(int x){
-    Node* p = new Node;
-    p->info =x ;
+    Node *Head, *Tail;
+};  
+Node* GetNode(int x){
+    Node *p = new Node;
+    p->info = x;
     p->next = NULL;
     return p;
 };
-void addHead(List &L,int x){
+void addHead(List &L, int x){
     Node *p = GetNode(x);
     if(L.Head == NULL){
-        L.Head = L.Tail= p;
+        L.Head = L.Tail = p;
     }
-    else {
+    else{
         p->next = L.Head;
         L.Head = p;
     }
 }
 void addTail(List &L, int x){
-    Node*p = GetNode(x);
+    Node *p = GetNode(x);
     if(L.Tail == NULL){
         L.Head = L.Tail = p;
     }
     else {
-       L.Tail->next = p;
-       L.Tail = p;
+        L.Tail->next = p;
+        L.Tail = p;
     }
 }
-Node* searchEle(List L, int x){
-    Node *k = L.Head;
-    while(k!=NULL){
-        if(k->info == x){
-            return k;
+Node* searchEle(List L, int key){
+    Node *p = L.Head;
+    while(p != NULL){
+        if(p->info == key){
+            return p;
         }
-        k = k->next;
+        p=p->next;
     }
     return L.Head;
 }
-void addAfter(List &L,int x, int y){
-    Node *k = searchEle(L,x);
-    if(k->next == NULL){
-        addTail(L,y);
-    }
-    else if(k == L.Head){
+void AddAfter(List &L, int x, int y){
+    Node *p = searchEle(L,x);
+    Node* newnode = GetNode(y);
+    if(p==L.Head){
         addHead(L,y);
     }
     else {
-        Node *new_node = GetNode (y);
-        new_node->next = k->next;
-        k->next = new_node;
+        newnode->next = p->next;
+        p->next = newnode;
     }
 }
 void Print(List L){
-    Node*p;
-    p=L.Head;
-    while(p!=NULL)
-    {
-        cout<<p->info<<" ";  
+    Node *p = L.Head;
+    while(p!= NULL){
+        cout<<p->info<<" ";
         p=p->next;
     }
 }
 int main(){
     List L;
     L.Head = L.Tail = NULL;
+    int x;
     while(1){
-        int x;
         cin>>x;
+        if(x==3){
+            break;
+        }
         switch(x){
-            case 0: {
-                int y;cin>>y;addHead(L,y);break;
+            case 0:{
+                int y;
+                cin>>y;
+                addHead(L,y);
+                break;
             }
             case 1:{
-                int y; cin>>y; addTail(L,y);break;
+                int y;cin>>y;
+                addTail(L,y);
+                break;
             }
             case 2:{
                 int y,z;
                 cin>>y>>z;
-                addAfter(L,y,z);
+                AddAfter(L,y,z);
                 break;
             }
         }
