a9502c94d7b6d4873e36da85dcad2b2251087644
CuongNguyen <c.ncuongnguyen2708@gmail.com>
2024-09-24 22:25:15 +0700

    committed

--- Sort/Merge_Sort.cpp
+++ Sort/Merge_Sort.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 void inputArray(int *a, int n){
+    a = new int [n];
     for(int i = 0 ; i < n ;i ++){
         cin>>a[i];
     }
@@ -10,8 +11,40 @@ void outputArray(int *a, int n){
         cout<<a[i] << " ";
     }
 }
-void MergeSort(int *a, int n){
-    int l,r;
+void Merge(int *b, int *c , int *a){
+    int leftsize = (sizeof(a)/sizeof(a[0])/2);
+    int rightsize = (sizeof(a)/sizeof(a[0])) - leftsize;
+    int i = 0, l = 0, r = 0;
+    while(l < leftsize && r < rightsize){
+        if(b[l]<c[r]){
+            a[i++] = b[l++];
+        }
+        else {
+            a[i++] = c[r++];
+        }
+    }
+    while(l<leftsize){a[i++] = b[l++];}
+    while(r<rightsize){a[i++] = c[r++];}
+}
+void MergeSort(int *a,int n){
+    int length = sizeof(a)/sizeof(a[0]);
+    if(length <= 1){
+        return;
+    }
+    int mid = length/2;
+    int *b = new int [mid];
+    int *c = new int [length - mid];
+    int i = 0, j = 0;
+    for(i;i<length;i++){
+        if(i < mid){
+            b[i] = a[i];
+        }
+        else {
+            c[j] = a[i];
+            j++;
+        }
+    }
+    Merge(b,c,a);
 }
 int main(){
     int *a, n;
